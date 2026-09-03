ae64d677fdb7d045daa974d69633eae80a8b2ce0
Alisha-Batool <javeed019101@gmail.com>
2026-09-04 01:41:24 +0500

    Remove unsupported i18n locales
    Keep the registered translations aligned with the available language choices.

--- web/src/i18n/index.js
+++ web/src/i18n/index.js
@@ -4,9 +4,7 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import en from './locales/en.json';
 import ur from './locales/ur.json';
 import urRoman from './locales/ur-roman.json';
-import pa from './locales/pa.json';
 import ps from './locales/ps.json';
-import sd from './locales/sd.json';
 
 i18n
   .use(LanguageDetector)
@@ -16,9 +14,7 @@ i18n
       en: { translation: en },
       ur: { translation: ur },
       'ur-roman': { translation: urRoman },
-      pa: { translation: pa },
       ps: { translation: ps },
-      sd: { translation: sd },
     },
     fallbackLng: 'en',
     interpolation: { escapeValue: false },
